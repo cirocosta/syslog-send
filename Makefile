0f37b52c341280f39dddc42ef3d8195e32dfceb0
Ciro S. Costa <ciro9758@gmail.com>
2018-08-22 18:09:35 -0400

    Includes deps; makes possible to send a message

--- Makefile
+++ Makefile
@@ -1,2 +1,7 @@
+all: install
+
 run:
 	docker-compose up --build
+
+install:
+	go install -v ./cmd/sample-syslog
--- cmd/sample-syslog/main.go
+++ cmd/sample-syslog/main.go
@@ -1,1 +1,46 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"log/syslog"
+	"strings"
+
+	"github.com/jessevdk/go-flags"
+)
+
+var opts struct {
+	Address   string `long:"address" description:"address of the syslog server" default:"127.0.0.1:514"`
+	Transport string `long:"transport" description:"transport to use (tcp|udp)" default:"udp"`
+	Args      struct {
+		Message []string
+	} `positional-args:"yes" required:"yes"`
+}
+
+func must(err error) {
+	if err == nil {
+		return
+	}
+
+	log.Fatal(err)
+}
+
+func main() {
+	_, err := flags.Parse(&opts)
+	must(err)
+
+	switch opts.Transport {
+	case "udp":
+	case "tcp":
+	default:
+		log.Fatal(fmt.Errorf("Unknown transport %s", opts.Transport))
+	}
+
+	logger, err := syslog.Dial(opts.Transport, opts.Address,
+		syslog.LOG_LOCAL0|syslog.LOG_USER, "custom-tag")
+	must(err)
+
+	defer logger.Close()
+
+	fmt.Fprintf(logger, strings.Join(opts.Args.Message, " "))
+}
